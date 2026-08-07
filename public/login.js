b498b01672895419bcda54b1446481ff60e255a9
Eazybel <eazybel27@gmail.com>
2026-08-08 00:20:48 +0300

    feat: add Firebase initialization and captcha verification to login functionality

--- public/login.js
+++ public/login.js
@@ -1,7 +1,25 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyAXviVipDAJZl-xyiQE4JfACkcl1xt_CqM",
+    authDomain: "hagere-c6abc.firebaseapp.com",
+    projectId: "hagere-c6abc",
+    storageBucket: "hagere-c6abc.firebasestorage.app",
+    messagingSenderId: "272791886594",
+    appId: "1:272791886594:web:05b9f9fd6fc98cdca3c01b"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
 const logInButton=document.getElementById("logInButton")
+const captchaVerifyDiv=document.getElementById("captchaVerifyDiv")
 const otpButton=document.getElementById("otpButton")
 const otpInput=document.getElementById("otp-code")
-const phoneNumberInput=document.getElementById("phone-number")
 logInButton.onclick=()=>{
+    const phoneNumberInput=document.getElementById("phone-number").value
     console.log(phoneNumberInput)
+}
+const render=()=>{
+    window.recaptchaVerifier=new firebaseConfig.authDomain.RecaptchaVerifier("captchaVerifyDiv")
+    recaptchaVerifier.render()
 }
