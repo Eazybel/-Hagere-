e77df92e68dbdf5b699824f199f99290a62ab4de
Eazybel <eazybel27@gmail.com>
2026-08-08 15:05:18 +0300

    feat: implement admin authentication page with email and password login; initialize Firebase in adminAuth.js

---
+++ public/adminAuth.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyBg9v5J4qdDYQhYCl79Q1U5Nm7hzrQ7yMw",
+    authDomain: "hagere-admin.firebaseapp.com",
+    projectId: "hagere-admin",
+    storageBucket: "hagere-admin.firebasestorage.app",
+    messagingSenderId: "907064312662",
+    appId: "1:907064312662:web:e716e0df19627c4a5dccf7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
