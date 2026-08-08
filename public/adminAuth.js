bcd2ed89507314acd683a77e76426f6bb394f750
Eazybel <eazybel27@gmail.com>
2026-08-08 15:28:49 +0300

    feat: implement admin login functionality and update navigation links to admin authentication page

--- public/adminAuth.js
+++ public/adminAuth.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
   const firebaseConfig = {
     apiKey: "AIzaSyBg9v5J4qdDYQhYCl79Q1U5Nm7hzrQ7yMw",
     authDomain: "hagere-admin.firebaseapp.com",
@@ -8,3 +9,24 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/fireba
     appId: "1:907064312662:web:e716e0df19627c4a5dccf7"
   };
   const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+// VARIABLE INITIATION
+const logInBtn=document.getElementById("logInBtn")
+const adminEmail=document.getElementById("adminEmail")
+const adminPassword=document.getElementById("adminPassword")
+logInBtn.onclick=()=>{
+console.log(adminEmail.value, adminPassword.value)
+const email=adminEmail.value
+const password=adminPassword.value
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+   console.log("user verified")
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+     console.log("user not verified")
+  });
+}
