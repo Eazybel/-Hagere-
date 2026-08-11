cfe3e128acf5f9ac49c5aff75bdc569555bf8b39
Eazybel <eazybel27@gmail.com>
2026-08-11 20:31:31 +0300

    feat: enhance publish modal with form handling and submit functionality

--- public/admin.js
+++ public/admin.js
@@ -8,13 +8,18 @@
     messagingSenderId: "272791886594",
     appId: "1:272791886594:web:22290488e32074b8a3c01b"
   };
-
+//VARIABLE AND FIREBASE INITIATER
   const app = initializeApp(firebaseConfig);
   const auth=getAuth(app)
   const publishModal=document.getElementById("publishModal")
   const publishBtn=document.getElementById("publishBtn")
   const cancelBtn=document.getElementById("cancelBtn")
-  publishBtn.onclick=async()=>{
+  const submitBtn=document.getElementById("submitBtn")
+  const publishForm=document.getElementById("publishForm")
+  onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // MODAL OOPENER AND CLOSER
+ publishBtn.onclick=async()=>{
     publishModal.classList.remove("hidden")
     document.querySelector("main").classList.add("hidden")
   }
@@ -22,5 +27,20 @@
     document.querySelector("main").classList.remove("hidden")
     publishModal.classList.add("hidden")
   }
+// SUBMIT HANDLER
+submitBtn.onclick=(e)=>{
+  e.preventDefault()
+  const form=new FormData(publishForm)
+  const objectedForm=Object.fromEntries(form.entries())
+  console.log(objectedForm)
+  
+}
+    const uid = user.uid;
+    // ...
+  } else {
+console.log("user is not signed up")
+  }
+})
+ 
 
 
