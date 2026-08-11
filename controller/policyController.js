a0aa5227893849586af235770bafbf97219a5d26
Eazybel <eazybel27@gmail.com>
2026-08-11 22:35:57 +0300

    feat: refactor policy update logic to improve error handling and utilize request body for policy creation

--- controller/policyController.js
+++ controller/policyController.js
@@ -2,31 +2,32 @@
 const {cloudinary, upload}=require("../utils/cloudinary")
 const policyModel=require("../models/Policy")
 const policyUpdate=(async(req,res)=>{
-//     if(!req.file){
-//     res.status(404).send("File upload not found")
-//     }
-//     try{
-// const uploadStream=cloudinary.uploader.upload_stream({
-//     folder:"policyFiles",
-//     resource_type:"auto"
+    if(!req.file){
+    res.status(404).send("File upload not found")
+    }
+    try{
+const uploadStream=cloudinary.uploader.upload_stream({
+    folder:"policyFiles",
+    resource_type:"auto"
 
-// },(error,result)=>{
-//     if(error){
-//         res.send(401).send(error)
-//     }
-//     res.status(200).json(result)
-// })
-// uploadStream.end(req.file.buffer)
-//     }catch(error){
-// console.log(error)
-//     }
-const newPolicy=new policyModel({
-    title:"title",
-    category:"category",
-    summary:"summery",
-    pdfUrl:"url"
+},async(error,result)=>{
+    if(error){
+        res.send(401).send(error)
+    }
+    const newPolicy=new policyModel({
+    title:req.body.title,
+    category:req.body.sector,
+    summary:req.body.description,
+    pdfUrl:result.url
 })
 await newPolicy.save().then(()=>console.log("saved"))
-res.json(req.file)
+    res.status(200).json(result)
+})
+uploadStream.end(req.file.buffer)
+    }catch(error){
+console.log(error)
+    }
+
+// res.json(req.file)
 })
 module.exports=policyUpdate
