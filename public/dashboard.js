1d762cfb93060fd715a677b53bc19e9ab659e0a2
Eazybel <eazybel27@gmail.com>
2026-08-16 23:38:26 +0300

    feat: update dashboard header to include Amharic name and enhance sign-out functionality

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
- import { getAuth, signInWithPopup, GoogleAuthProvider , onAuthStateChanged }  from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+ import { getAuth, signInWithPopup, GoogleAuthProvider , onAuthStateChanged, signOut }  from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
   const firebaseConfig = {
     apiKey: "AIzaSyAXviVipDAJZl-xyiQE4JfACkcl1xt_CqM",
     authDomain: "hagere-c6abc.firebaseapp.com",
@@ -8,18 +8,24 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/fireba
     messagingSenderId: "272791886594",
     appId: "1:272791886594:web:05b9f9fd6fc98cdca3c01b"
   };
-
+// VARIABLE INITIATION FOR DOM AND OTHER
+const signOutButton=document.getElementById("signOutButton")
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const auth=getAuth(app)
 onAuthStateChanged(auth, (user) => {
   if (user) {
-console.log(user
-    
-)
+console.log(user)
     const uid = user.uid;
     // ...
   } else {
 window.location.href="./login.html"
   }
 });
+signOutButton.onclick=()=>{
+  signOut(auth).then(() => {
+    window.location.href("./index.html")
+}).catch((error) => {
+  console.log(error)
+});
+}
